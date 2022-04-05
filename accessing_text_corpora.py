a3410f5c5cc77045f7b08a7f5db0ab2398e917db
Gian Piero <gianpierosantini@gmail.com>
2022-04-05 10:15:04 +0200

    import chatroom corpus

--- accessing_text_corpora.py
+++ accessing_text_corpora.py
@@ -21,3 +21,6 @@ from nltk.corpus import webtext
 for fileid in webtext.fileids():
     print(fileid, webtext.raw(fileid)[:65], '...')
 
+from nltk.corpus import nps_chat
+chatroom = nps_chat.posts('10-19-20s_706posts.xml')
+chatroom[123]
